0909fba9e2feb1d46287c139dd1a570b23505b1b
Rob Marty <ramarty@email.wm.edu>
2019-02-20 12:38:21 -0500

    Update readme

--- Python/README.md
+++ Python/README.md
@@ -2,17 +2,21 @@
 
 `gee_functions.py` contains functions for working with GEE in python.
 
-**download_viirs** *(country, begin_date, end_date, reduce_type, directory, name)*
+**download_image_aggregate** *(image_type, country, begin_date, end_date, reduce_type, directory, name, cloud_cover_filter)*
 
-  Downloads a .tif file of nighttime lights from VIIRS. Aggregates monthly tiles over selected date range.
+Downloads a .tif file of satellite image. I multiple images across time, aggregates images into one image.
 
-  - *country:* Name of Country (e.g., 'Rwanda')
-  - *begin_date:* Begin date of images
-  - *end_date:* End date of images
-  - *reduce_type:* Reduce type of images ('mean', 'median' or 'max')
-  - *directory:* Directory where to save new tile
-  - *name:* Name of new file (e.g., 'new_raster.tif')
+- *image_type:* Image source and type (options: viirs, dmspols, modis_ndvi, modis_evi, l8_ndvi)
+- *country:* Name of Country (e.g., 'Rwanda')
+- *begin_date:* Begin date of images
+- *end_date:* End date of images
+- *reduce_type:* Reduce type of images ('mean', 'median' or 'max')
+- *directory:* Directory where to save new tile
+- *name:* Name of new file (e.g., 'new_raster.tif')
+- *cloud_cover_filter:* Removes images with a cloud cover greater than percent (default = 50)
 
-  Example:
+Examples:
 
-  `download_viirs('Rwanda', '2015-01-01', 2015-12-31, 'median', 'Users/myname/Desktop/', 'viirs_rwa_2015_median.tif')`
+    download_image_aggregate("viirs", "Rwanda", "2015-01-01", "2015-12-31", "mean", "/Users/robmarty/Desktop/test", 'rwa_viirs_2015.tif')
+    download_image_aggregate("modis_evi", "Rwanda", "2010-01-01", "2010-12-31", "mean", "/Users/robmarty/Desktop/test", 'rwa_modis_evi_2010.tif')
+    download_image_aggregate("l8_ndvi", "Rwanda", "2018-01-01", "2018-12-31", "mean", "/Users/robmarty/Desktop/test", 'rwa_l8_ndvi_2018.tif')
--- Python/gee_functions.py
+++ Python/gee_functions.py
@@ -6,6 +6,8 @@ import time
 import zipfile
 ee.Initialize()
 
+# TODO: Delete zip file from downloads folder.
+
 # https://stackoverflow.com/questions/35851281/python-finding-the-users-downloads-folder
 def get_download_path():
     """Returns the default downloads path for linux or windows"""
@@ -19,37 +21,68 @@ def get_download_path():
     else:
         return os.path.join(os.path.expanduser('~'), 'downloads')
 
-def download_viirs(country, begin_date, end_date, reduce_type, directory, name):
+def download_image_aggregate(image_type, country, begin_date, end_date, reduce_type, directory, name, cloud_cover_filter=50):
     """Downloads VIIRS Image for a Country"""
 
     directory = os.path.abspath(directory)
 
-    # Grab Country
+    # Grab Country -------------------------------------------------------------
     countries = ee.FeatureCollection('ft:1tdSwUL7MVpOauSgRzqVTOwdfy17KDbw-1d9omPw');
     country = countries.filter(ee.Filter.eq('Country', country));
 
-    # Grab image, filter by date and country. Take median value across bands
-    image = ee.ImageCollection('NOAA/VIIRS/DNB/MONTHLY_V1/VCMCFG')
+    # Grab and Proccess Image --------------------------------------------------
+    # Grab image and select bands
+    if(image_type == "viirs"):
+        image = ee.ImageCollection('NOAA/VIIRS/DNB/MONTHLY_V1/VCMCFG')
+        image = image.select('avg_rad')
+        scale = 750
+
+    if(image_type == "dmspols"):
+        image = ee.ImageCollection('NOAA/DMSP-OLS/NIGHTTIME_LIGHTS')
+        image = image.select('stable_lights')
+        scale = 1000
+
+    if(image_type == "modis_ndvi"):
+        image = ee.ImageCollection('MODIS/006/MOD13Q1')
+        image = image.select('NDVI')
+        scale = 250
+
+    if(image_type == "modis_evi"):
+        image = ee.ImageCollection('MODIS/006/MOD13Q1')
+        image = image.select('EVI')
+        scale = 250
+
+    if(image_type == "l8_ndvi"):
+        image = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA');
+        image = ee.ImageCollection(image.filter(ee.Filter.lt('CLOUD_COVER', cloud_cover_filter)))
+        scale = 250
+
+    # Filter by dates
     image = ee.ImageCollection(image.filterDate(begin_date,end_date))
 
-    # Reduce images in image collection; select only radiance band
+    # Reduce images in image collection
     if(reduce_type == "median"):
         image = image.reduce(ee.Reducer.median())
-        image = image.select('avg_rad_median')
+        #image = image.select('avg_rad_median')
 
     if(reduce_type == "mean"):
         image = image.reduce(ee.Reducer.mean())
-        image = image.select('avg_rad_mean')
+        #image = image.select('avg_rad_mean')
 
     if(reduce_type == "max"):
         image = image.reduce(ee.Reducer.max())
-        image = image.select('avg_rad_max')
+        #image = image.select('avg_rad_max')
+
+    # If landsat or sentinel, need to compute indices
+    if(image_type == "l8_ndvi"):
+        image = image.expression('float(b("B5_'+reduce_type+'") - b("B4_'+reduce_type+'")) / (b("B5_'+reduce_type+'") + b("B4_'+reduce_type+'"))')
 
     # Clip to country
     image = image.clip(country)
 
+    # Download Image -----------------------------------------------------------
     path = image.getDownloadUrl({
-        'scale': 30,
+        'scale': scale,
         'crs': 'EPSG:4326',
         'region': country.geometry().getInfo()['coordinates']
     })
@@ -69,9 +102,7 @@ def download_viirs(country, begin_date, end_date, reduce_type, directory, name):
         time.sleep(2)
 
     files_after = glob.glob(get_download_path() + "/*.zip")
-
     zip_file = list(set(files_after) - set(files_initial))
-    zip_file[0]
 
     # Rename and Send Files to Folder
     dir_files_initial = glob.glob(directory + "/*")
@@ -90,6 +121,12 @@ def download_viirs(country, begin_date, end_date, reduce_type, directory, name):
     # Rename .tif file
     os.rename(extracted_files[1], os.path.abspath(directory + "/" + name))
 
+    # Delete zip file
+    os.remove(zip_file[0])
+
     print("Done! File saved to: " + os.path.abspath(directory + "/" + name))
 
-download_viirs("Rwanda", "2015-01-01", "2015-12-31", "mean", "/Users/robmarty/Desktop/test", 'rwa_viirs_2015.tif')
+# Examples
+#download_image_aggregate("viirs", "Rwanda", "2015-01-01", "2015-12-31", "mean", "/Users/robmarty/Desktop/test", 'rwa_viirs_2015.tif')
+#download_image_aggregate("modis_evi", "Rwanda", "2010-01-01", "2010-12-31", "mean", "/Users/robmarty/Desktop/test", 'rwa_mevi_2015.tif')
+#download_image_aggregate("l8_ndvi", "Rwanda", "2018-01-01", "2018-12-31", "mean", "/Users/robmarty/Desktop/test", 'rwa_l8_ndvi_2018_v2.tif')
